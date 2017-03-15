66ebca0dcd45f80149b2d22b023d562c12ba9456
agadrius <lessire.florent@gmail.com>
2017-03-15 12:07:29 +0100

    creation du header

--- include/header.php
+++ include/header.php
@@ -1,6 +1,8 @@
 <header class="row">
-			<img class="col-md-12"id="logo"><!-- logo speedmove -->
+			<img src="images/logo.png" alt="logo_speedmove" class="col-md-4"id="logo"><!-- logo speedmove -->
 			</img>
-			<img class="col-md-12" id="contact"> <!-- logo tel + num -->
+			<div class="col-md-2">
+			</div>
+			<img src="images/telephone.png" alt="logo_speedmove" class="col-md-6" id="contact"> <!-- logo tel + num -->
 			</img>
 		</header>
