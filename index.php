f642e7fd9fb979f23fd25ca9e849fed568acd135
agadrius <lessire.florent@gmail.com>
2017-03-15 10:03:47 +0100

    creation index.php + section

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <title>Speed-Move, taxi de la semois</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- bootstrap core css -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+	<!-- custome template -->
+    <link href="css/.css" rel="stylesheet">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+	<div class="container">
+		<?php include 'include/header.php';?><!-- header includes -->
+		<!-- slider -->
+		<?php include 'include/slider.php';?><!-- slider includes -->
+		<!-- navigation -->
+		<?php include 'include/menu.php';?><!-- menu includes -->
+		<!-- corps de la page -->
+		<div class="row" id="corps_page">
+			<section class="col-lg-12" id="services"><!-- services -->
+			</section>
+			<section class="col-lg-12" id="contact"><!-- contact -->
+			</section>
+			<section class="col-lg-12" id="joint_us"><!-- nous rejoindre -->
+			</section>
+		</div>
+		<!-- footer -->
+		<?php include 'include/footer.php';?><!-- footer includes -->		
+	</div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
