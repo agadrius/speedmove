5cd301e9be17ed2503ed94be50611297babb6dad
agadrius <lessire.florent@gmail.com>
2017-03-15 12:37:51 +0100

    creation du carroussel

--- include/header.php
+++ include/header.php
@@ -1,8 +1,8 @@
 <header class="row">
-			<img src="images/logo.png" alt="logo_speedmove" class="col-md-4"id="logo"><!-- logo speedmove -->
+			<img src="images/logo.png" alt="logo_speedmove" class="col-sm-4 col-xs-12"id="logo"><!-- logo speedmove -->
 			</img>
-			<div class="col-md-4">
+			<div class="col-sm-4">
 			</div>
-			<img src="images/telephone.png" alt="logo_speedmove" class="col-md-4" id="contact"> <!-- logo tel + num -->
+			<img src="images/telephone.png" alt="logo_speedmove" class="col-sm-4 col-xs-12" id="contact"> <!-- logo tel + num -->
 			</img>
 		</header>
--- include/slider.php
+++ include/slider.php
@@ -1,3 +1,16 @@
 <div class="row" id="slider">
+	<div id="carousel" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="item active"> <img alt="" src="images/multivan.jpg"></div>
+    <div class="item"> <img alt="" src="images/multivanetremorque.jpg"></div>
+    <div class="item"> <img alt="" src="images/remorque.jpg"></div>
+  </div>
+  <a class="left carousel-control" href="#carousel" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+  </a>
+  <a class="right carousel-control" href="#carousel" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+  </a>
+</div>
 
 </div>
