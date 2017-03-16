f560e22c1cacd207b83af1703e565cc6fe864a03
agadrius <lessire.florent@gmail.com>
2017-03-16 11:25:38 +0100

    modification du lien acceuil dans le menu

--- include/menu.php
+++ include/menu.php
@@ -10,7 +10,7 @@
     </div>
     <div id="navbar" class="collapse navbar-collapse">
       <ul class="nav navbar-nav" >
-        <li><a href="#">Accueil</a></li>
+        <li><a href="index.php">Accueil</a></li>
 		<li class="dropdown"> 
             <a data-toggle="dropdown" href="#">Services<b class="caret"></b></a>
             <ul class="dropdown-menu">
