10ee035d20da17ee2a48346fc878399dba187503
agadrius <lessire.florent@gmail.com>
2017-03-20 12:23:10 +0100

    modification et changement de couleur text footer

--- include/footer.php
+++ include/footer.php
@@ -1,13 +1,13 @@
 <footer class="row" id="footer">
 	<div class="container">
 		<div class="col-md-3">
-			<h3>Nos Informations</h3>
-			<p>bla</br>
-			bla</br>
-			bla</br>
-			bla</br>
-			bla</br>
-			bla</br></p>
+			<h3>Coordonnées</h3>
+			<p>Speed-Move</br>
+			Place Paul Dubois 198</br>
+			5550 sugny</br>
+			N°taxi :</br>
+			+32(0)471/51.56.65</br>
+			tva 0597-698-261</br></p>
 		</div>
 		<div class="col-md-6">
 		</div>
