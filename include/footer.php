0b69e9d29747a6d296fcd2ad62197700ed994a86
agadrius <lessire.florent@gmail.com>
2017-03-20 13:42:18 +0100

    amélioration des espace vide

--- include/footer.php
+++ include/footer.php
@@ -9,9 +9,7 @@
 			+32(0)471/51.56.65</br>
 			tva 0597-698-261</br></p>
 		</div>
-		<div class="col-md-6">
-		</div>
-		<div class="col-md-3">
+		<div class="col-md-offset-6 col-md-3">
 			<h3>Nous rejoindre</h3>
 			<a href="#"><i class="fa fa-facebook fa-2x" aria-hidden="true"></i></a><!-- logo fb -->
 		</div>
--- include/header.php
+++ include/header.php
@@ -1,8 +1,6 @@
 <header class="row">
 	<img src="images/logo.png" alt="logo_speedmove" class="col-lg-4 col-sm-6 col-xs-12"id="logo"><!-- logo speedmove -->
 		</img>
-	<div class="col-lg-4">
-	</div>
-	<img src="images/telephone.png" alt="logo_speedmove" class="col-lg-4 col-sm-6 col-xs-12" id="contact"> <!-- logo tel + num -->
+	<img src="images/telephone.png" alt="logo_speedmove" class="col-lg-offset-4 col-lg-4 col-sm-6 col-xs-12" id="contact"> <!-- logo tel + num -->
 	</img>
 </header>
