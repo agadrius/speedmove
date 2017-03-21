d47f4b919d95d25003d04ea234af21d2f0fea958
agadrius <lessire.florent@gmail.com>
2017-03-21 11:03:04 +0100

    clone service navette et renomage service livraison

---
+++ service-livraison.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <title>Speed-Move, taxi de la semois</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- lib icone-->
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+	<!-- bootstrap core css -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+	<!-- custome template -->
+    <link href="css/style.css" rel="stylesheet">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+	<div class="container">
+		<?php include 'include/header.php';?><!-- header includes -->
+		<!-- navigation -->
+		<?php include 'include/menu.php';?><!-- menu includes -->
+
+		<!-- corps de la page -->
+		<section class="row">
+			<div class="col-md-8" id="txt_service_navette">
+				<h1>Service de <strong>navette</strong> <strong>aéroport</strong> ou <strong>gares</strong></h1>
+				<p>Pour tous vos besoins de navette vers un <strong>aéroport</strong> ou une <strong>gare</strong> en Belgique ou pays frontalier, <strong>Speed-Move</strong> vous accompagne de votre porte à la destination que vous avez choisie.</p>
+				<p>Ainsi pour faciliter vos déplacements, Speed-Move vous propose ses services tout en vous garantissant le confort du voyage (<em>Zaventem</em>, <em>Liège</em>, <em>Charleroi</em>, <em>Luxembourg</em>, <em>Paris</em>, …).</p>
+				<p>Idéal pour un trajet dans les meilleures conditions possibles: pas de stress, pas de frais de parking, de péage, sécurité de votre véhicule et surtout un tarif attractif.</p>
+				<span class="important"><p>Pour un maximum d’efficacité, veuillez préparer les informations suivantes:</p></span>
+				<ul>
+					<li>Vos nom et prénom</li>
+					<li>Nombre de passager, d’enfants</li>
+					<li>Lieu, date et heure de la prise en charge</li>
+					<li>Votre Destination</li>
+					<li>Nombre de valises</li>
+				</ul>
+			</div>
+			<div class="col-md-4" id="img_service_navette">
+				<img src="images/service-navette1-md.jpg" alt="service_navette_1">
+				<img src="images/navette2-md.jpg" alt="service_navette_2">
+			</div>
+		</section>
+			
+	</div>
+	</div>
+		<!-- footer -->
+		<?php include 'include/footer.php';?><!-- footer includes -->		
+	
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
