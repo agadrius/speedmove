4e835ee748fb0685bc5acee1c247a93f2c83dceb
agadrius <lessire.florent@gmail.com>
2017-03-21 09:52:10 +0100

    clone de service taxi et renomage en service navette

---
+++ service-navette.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <title>Speed-Move, taxi de la semois</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- lib icone-->
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+	<!-- bootstrap core css -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+	<!-- custome template -->
+    <link href="css/style.css" rel="stylesheet">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+	<div class="container">
+		<?php include 'include/header.php';?><!-- header includes -->
+		<!-- navigation -->
+		<?php include 'include/menu.php';?><!-- menu includes -->
+
+		<!-- corps de la page -->
+		<section class="row">
+			<div class="col-md-8" id="txt_service_navette">
+				<h1>Service de <strong>taxi</strong></h1>
+				<p>Besoin d’un <strong>taxi</strong> rapidement? Appelez-nous. Nos services de taxi sont valables dans toute la Belgique.</br>
+				<em>Sugny</em> | <em>Vresse-Sur-Semois</em> | <strong>Bouillon</strong> | <em>Gedinne</em> | <em>Bièvre</em>  | <em>Graide</em> | <em>Carlsbourg</em> | <em>Bertrix</em> | <em>Libramont</em> | …</br>
+				De jour comme de nuit notre service peut répondre à votre attente dans les plus brefs délais. N’hésitez pas à nous contacter. Profitez de notre véhicule haut de gamme. Celui-ci peut accueillir jusqu’à 6 personnes. Ce qui réduit le coût par personne.</p>
+				<h2><strong>Soirée ou restaurant</strong></h2>
+				<p>Une soirée un peu trop arrosée ? Une virée entre amis ? <strong>Speed-Move</strong> est là pour l’aller comme pour le retour.</br>
+				Pour un maximum d’efficacité, veuillez préparer les informations suivantes:</p>
+				<ul>
+				   <li>Vos nom et prénom</li>
+				   <li>Nombre de passager, d’enfants</li>
+				   <li>Lieu, date et heure de la prise en charge</li>
+				   <li>Votre Destination</li>
+				</ul>
+			</div>
+			<div class="col-md-4" id="img_service_navette">
+				<img src="images/mulitvan3-md.jpg" alt="service_taxi_1">
+				<img src="images/service-taxi2-md.jpg" alt="service_taxi_2">
+			</div>
+		</section>
+			
+	</div>
+	</div>
+		<!-- footer -->
+		<?php include 'include/footer.php';?><!-- footer includes -->		
+	
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
