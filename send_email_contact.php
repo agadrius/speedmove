ed9e6958ea58dd9b94e0f889b3f8a72084198de7
agadrius <lessire.florent@gmail.com>
2017-03-23 12:28:55 +0100

    mofication du message mail

---
+++ send_email_contact.php
@@ -0,0 +1,54 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+  <title>Speed-move</title>
+  <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+<!-- header -->
+
+<!-- navigation et menu -->
+
+<!-- section -->
+<section>
+	<!-- article -->
+	<article>
+		<?php
+ 
+// Mettez ici votre adresse valide
+$to = "lessire.florent@gmail.com";
+ 
+// Sujet du message 
+$suject = "demande de contact" ;
+$nom = $_POST['nom'] ;
+$tel= $_POST['telephone'] ;
+$email = $_POST['email'] ;
+$message = $_POST['message'] ;
+// Corps du message, écrit en texte et encodage iso-8859-1
+
+$message_email = "Bonjour,\n un message vous a ete envoye depuis speed-move.be \n\n\n information du contact : \n contact: " .$nom. "\n telephone : " .$tel. "\n email : " .$email. "\n\n voici son message : \n" .$message. "\n";
+ 
+// Entêtes du message
+$headers = ""; // on vide la variable
+$headers = "From: " .$nom. " <" .$email. ">\n"; // ajout du champ From
+$headers = $headers."MIME-Version: 1.0\n"; // ajout du champ de version MIME
+$headers = $headers."Content-type: text/plain; charset=iso-8859-1\n"; // ajout du type d'encodage du corps
+ 
+// Appel à la fonction mail
+if ( mail($to, $suject, $message_email, $headers) == TRUE )
+{
+   echo "Envoi du mail reussi.";
+}
+else
+{
+   echo "Erreur : l'envoi du mail a échoué.";
+}
+ 
+?>
+	</article>
+	<!-- aside -->
+</section>
+<!-- footer -->
+
+</body>
+</html>
