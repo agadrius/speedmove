a97bd1fdb8fb5157e494323f50abbe70f8e5797b
agadrius <lessire.florent@gmail.com>
2017-03-29 10:25:34 +0200

    ajout du lien tel

--- include/header.php
+++ include/header.php
@@ -1,6 +1,6 @@
 <header class="row">
 	<img src="images/logo.png" alt="logo_speedmove" class="col-lg-4 col-sm-6 col-xs-12"id="logo"><!-- logo speedmove -->
 		</img>
-	<img src="images/telephone.png" alt="logo_speedmove" class="col-lg-offset-4 col-lg-4 col-sm-6 col-xs-12" id="contact"> <!-- logo tel + num -->
-	</img>
+	<a href="tel:0032471515665"><img src="images/telephone.png" alt="logo_speedmove" class="col-lg-offset-4 col-lg-4 col-sm-6 col-xs-12" id="contact"> <!-- logo tel + num -->
+	</img></a>
 </header>
