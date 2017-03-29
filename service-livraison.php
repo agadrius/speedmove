e48f7d6a85deb2c02fe833c1e436c46f208561e1
agadrius <lessire.florent@gmail.com>
2017-03-29 20:46:14 +0100

    suppression media querries

--- service-livraison.php
+++ service-livraison.php
@@ -44,9 +44,7 @@
 			
 	</div>
 		<!-- footer -->
-		<div class ="footer-xl">
 		<?php include 'include/footer.php';?><!-- footer includes -->	
-		</div>
 	
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
