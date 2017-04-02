50e7307f33739515c7cd5c97f05d977225451814
agadrius <lessire.florent@gmail.com>
2017-04-02 16:56:10 +0100

    definition var

--- contact.php
+++ contact.php
@@ -66,6 +66,15 @@
 	
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
-	
+	<script>
+	$(document).ready(function(){
+    
+		var $name = $('#name'),
+			$tel = $('#tel'),
+			$mail = $('#mail'),
+			$message = $('message');
+	});
+
+// </script>
   </body>
 </html>
