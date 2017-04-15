2940df7b58670589b8aa74097f323b6c8cbc2c48
agadrius <lessire.florent@gmail.com>
2017-04-15 15:02:44 +0100

    suppression lien logo

--- include/header.php
+++ include/header.php
@@ -1,5 +1,5 @@
 <header class="row">
-	<a href="index.php"><img src="images/logo.png" alt="logo_speedmove" class="col-lg-4 col-sm-6 col-xs-12"id="logo"></a><!-- logo speedmove -->
+	<img src="images/logo.png" alt="logo_speedmove" class="col-lg-4 col-sm-6 col-xs-12"id="logo"><!-- logo speedmove -->
 		</img>
 	<a href="tel:0032471515665"><img src="images/telephone.png" alt="logo_speedmove" class="col-lg-offset-4 col-lg-4 col-sm-6 col-xs-12" id="contact"> <!-- logo tel + num -->
 	</img></a>
